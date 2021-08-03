2bbfbe0fbbdf485946118f02eeb7088b73fce625
Julie Fitzpatrick <juliemfitzpatrick@gmail.com>
2021-08-03 18:43:28 +0000

    completed sumab recursive function

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
-
 function sum(fromN, toN) {
   // Sum all the values from fromN up to toN
-}
+  if (fromN < toN) {
+    return (fromN + sum((fromN + 1), toN));
+  }
+    return fromN;
+  };
+
 
 module.exports = sum;
+console.log(sum(1, 4)); //10
+console.log(sum(2, 4)); //9
+console.log(sum(13, 14)); //27
+console.log(sum(3, 7)); //27
+
